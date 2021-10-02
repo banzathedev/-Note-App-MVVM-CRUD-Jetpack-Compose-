2b16827bd5b9a33e8fbdde4089c4215f4f739b35
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-10-02 10:58:23 -0300

    Hilt Now provides, db, dao and uses cases

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.plcoding.cleanarchitecturenoteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDao
+import com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDatabase
+import com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.DeleteNoteUseCase
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNotesUseCase
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun providesNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun providesNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotesUseCase(repository),
+            deleteNote = DeleteNoteUseCase(repository)
+        )
+    }
+}
--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
@@ -12,4 +12,7 @@ abstract class NoteDatabase : RoomDatabase() {
 
     abstract val noteDao: NoteDao
 
+    companion object{
+        const val DATABASE_NAME = "notes_db"
+    }
 }
