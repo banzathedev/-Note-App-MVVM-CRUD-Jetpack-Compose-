201995351905441adf3de467b084aa93b8f5a725
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-10-02 09:56:59 -0300

    Note Model done

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,18 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.*
+
+@Entity
+data class Note(
+    val tittle: String,
+    val content: String,
+    val timesTamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink, LightBlue)
+    }
+}
