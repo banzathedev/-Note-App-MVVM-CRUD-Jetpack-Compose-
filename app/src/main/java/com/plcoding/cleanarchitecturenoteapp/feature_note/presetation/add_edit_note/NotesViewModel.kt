86bb367e85021892866703137cf9e6e12ccc1192
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-10-02 11:22:44 -0300

    Adding methods

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presetation/add_edit_note/NotesViewModel.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presetation/add_edit_note/NotesViewModel.kt
@@ -1,8 +1,13 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presetation.add_edit_note
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,15 +15,30 @@ class NotesViewModel @Inject constructor(
     private val notesUseCases: NoteUseCases
 ) : ViewModel() {
 
+    private val _state = mutableStateOf(NotesState())
+    val state: State<NotesState> = _state
+
+    private var recentlyDeletedNote: Note? = null
+
     fun onEvent(event: NotesEvent) {
         when (event) {
             is NotesEvent.Order -> {
             }
             is NotesEvent.DeleteNote -> {
+                viewModelScope.launch {
+                    notesUseCases.deleteNote(event.note)
+                    recentlyDeletedNote = event.note
+                }
             }
             is NotesEvent.RestoreNote -> {
+                viewModelScope.launch {
+
+                }
             }
             is NotesEvent.ToggleOrderSection -> {
+                _state.value = state.value.copy(
+                    isOderSectionVisible = !state.value.isOderSectionVisible
+                )
             }
         }
     }
