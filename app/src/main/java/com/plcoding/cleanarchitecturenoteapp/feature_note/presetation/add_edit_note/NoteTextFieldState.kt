759aec8cb6e6f479f800e1eeb9931f8431170636
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-10-05 15:41:50 -0300

    event class for crud done

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presetation/add_edit_note/NoteTextFieldState.kt
@@ -0,0 +1,8 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presetation.add_edit_note
+
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
+
