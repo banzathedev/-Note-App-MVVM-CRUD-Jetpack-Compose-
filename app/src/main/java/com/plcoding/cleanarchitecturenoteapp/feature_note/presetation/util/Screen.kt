074f56b1a05467d5996096dfb281312d211e5afb
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-10-05 18:02:26 -0300

    final touches done

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presetation/util/Screen.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presetation/util/Screen.kt
@@ -1,3 +1,6 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presetation.util
 
-sealed class Screen
+sealed class Screen(val route: String){
+    object NotesScreen: Screen("notes_screen")
+    object AddEditNoteScreen: Screen("add_edit_note_screen")
+}
