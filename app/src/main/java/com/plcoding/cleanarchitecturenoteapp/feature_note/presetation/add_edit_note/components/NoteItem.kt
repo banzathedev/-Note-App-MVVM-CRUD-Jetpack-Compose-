667155a5557e739ab0f8540226c32be3b4baf76a
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-10-05 14:11:31 -0300

    creating the retangles

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presetation/add_edit_note/components/NoteItem.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presetation/add_edit_note/components/NoteItem.kt
@@ -1,2 +1,48 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presetation.add_edit_note.components
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.ColorUtils
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+
+@Composable
+fun NoteItem(
+    note: Note,
+    modfier: Modifier = Modifier,
+    cornerRadius: Dp = 10.dp,
+    cutCornerRadius: Dp = 30.dp,
+    onDeleteClick: () -> Unit
+){
+    Box(modifier = modfier){
+        Canvas(modifier = Modifier.matchParentSize()){
+            val clipPath = Path().apply {
+                lineTo(size.width - cutCornerRadius.toPx(), 0f)
+                lineTo(size.width, cutCornerRadius.toPx())
+                lineTo(size.width, size.height)
+                lineTo(0f, size.height)
+                close()
+            }
+
+            clipPath(clipPath){
+                drawRoundRect(
+                    color = Color(note.color),
+                    size = size,
+                    cornerRadius = CornerRadius(cornerRadius.toPx())
+                )
+                drawRoundRect(
+                    color = Color(ColorUtils.blendARGB(note.color, 0x00000, 0.2f)),
+                    size = size,
+                    cornerRadius = CornerRadius(cornerRadius.toPx())
+                )
+            }
+        }
+    }
+}
