126a6769e37f0c9b92f30ac28d6dbd06842cddda
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-10-05 15:10:21 -0300

    delete undo impl
    jetpack compose its kinda funny, its like a language and css all in one, mixted fellings until now

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presetation/notes/NotesScreen.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presetation/notes/NotesScreen.kt
@@ -1,10 +1,15 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presetation.notes
 
+import androidx.compose.animation.*
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Sort
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -13,7 +18,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presetation.add_edit_note.components.NoteItem
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presetation.add_edit_note.components.OrderSection
+import kotlinx.coroutines.launch
 
+@ExperimentalAnimationApi
 @Composable
 fun NotesScreen(
     navController: NavController,
@@ -47,11 +56,55 @@ fun NotesScreen(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(text = "Your Note", style = MaterialTheme.typography.h4)
-                IconButton(onClick = { }) {
+                IconButton(
+                    onClick = {
+                        viewModel.onEvent(NotesEvent.ToggleOrderSection)
+                    },
+                ) {
+                    Icon(imageVector = Icons.Default.Sort, contentDescription = "Sort")
 
                 }
             }
+            AnimatedVisibility(
+                visible = state.isOderSectionVisible,
+                enter = fadeIn() + slideInVertically(),
+                exit = fadeOut() + slideOutVertically()
+            ) {
+                OrderSection(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    noteOrder = state.noteOrder,
+                    onOrderChanges = {
+                        viewModel.onEvent(NotesEvent.Order(it))
+                    }
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.notes) { note ->
+                    NoteItem(
+                        note = note,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
 
+                            },
+                        onDeleteClick = {
+                            viewModel.onEvent(NotesEvent.DeleteNote(note))
+                            scope.launch {
+                              val result = scaffoldState.snackbarHostState.showSnackbar(
+                                    message = "Note Deleted",
+                                    actionLabel = "Undo"
+                                )
+                                if(result == SnackbarResult.ActionPerformed) {
+                                    viewModel.onEvent(NotesEvent.RestoreNote)
+                                }
+                            }
+                        }
+                    )
+                }
+            }
         }
     }
 }
